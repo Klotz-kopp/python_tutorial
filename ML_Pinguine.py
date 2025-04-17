c4e5c9f1962f80c1e9512917e497c01eaf1644af
Klotz-kopp <158801159+klotz-kopp@users.noreply.github.com>
2025-04-17 18:34:06 +0200

    ML_Pinguine.py PEP8 konform angepasst.
    Signed-off-by: Klotz-kopp <wolters.simon@gmail.com>

--- ML_Pinguine.py
+++ ML_Pinguine.py
@@ -1,6 +1,4 @@
 #  Copyright (c) 2025. Diese Python Skripte wurden von mir erstellt und können als Referenz von anderen genutzt und gelesen werden.
-import numpy as np
-import pandas as pd
 import seaborn as sns
 from funktionen import printf
 from sklearn.model_selection import train_test_split
@@ -12,24 +10,26 @@ df = sns.load_dataset("penguins")
 # Null-Zeilen entfernen
 df = df.dropna()
 # nicht numerische Zeilen entfernen
-df = df.drop(["island", "sex"], axis = 1)
+df = df.drop(["island", "sex"], axis=1)
 # Features definiert
-X = df.drop(["species"], axis = 1)
+X = df.drop(["species"], axis=1)
 # Labels definiert
 y = df["species"]
 # Dataframe in Test und Trainingsdaten aufgeteilt
-X_train , X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 119)
-#Wir prüfen die Genauigkeit der Vorhersagen mit 1-10 Nachbarn
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=119)
+# Wir prüfen die Genauigkeit der Vorhersagen mit 1-10 Nachbarn
 genauigkeitswerte = []
-for i in range(1,11):
+for i in range(1, 11):
     # Classifier definiert
-    clf = KNeighborsClassifier(n_neighbors = i)
+    clf = KNeighborsClassifier(n_neighbors=i)
     # Classifier mit Trainingsdaten traininert
     clf.fit(X_train, y_train)
     # Vorhersagen mit dem Classifier auf die Testdaten durchgeführt
     predictions = clf.predict(X_test)
     # Vorhersagegenauigkeit berechnet, indem die Vorhersagen mit der realität verglichen wurde
-    printf(f"Bei der Verwendung des Wert {i} erreichen wir eine Genauigkeit von {accuracy_score(predictions, y_test)*100}%.")
-    genauigkeitswerte.append((i,accuracy_score(predictions, y_test)*100))
+    printf(
+        f"Bei der Verwendung des Wert {i} erreichen wir eine Genauigkeit von {accuracy_score(predictions, y_test) * 100}%.")
+    genauigkeitswerte.append((i, accuracy_score(predictions, y_test) * 100))
 hoechster_genauigkeitswert = max(genauigkeitswerte, key=lambda x: x[1])
-printf(f"Der höchste Genauigkeitswert der erreicht wurde liegt bei {hoechster_genauigkeitswert[1]}%, bei der betrachtung von {hoechster_genauigkeitswert[0]} Nachbarn.")
+printf(
+    f"Der höchste Genauigkeitswert der erreicht wurde liegt bei {hoechster_genauigkeitswert[1]}%, bei der betrachtung von {hoechster_genauigkeitswert[0]} Nachbarn.")
